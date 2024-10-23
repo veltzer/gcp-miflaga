bbd5da0343d7e78dd8f7344b2b1949182a2b86e5
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-23 11:51:52 +0300

    no commit message given

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import bisect
-import os
-import json
+"""
+Miflaga website to generate funny names for political parties in Israel
+"""
+
+
 import flask
 
 
 # the app object
-app = flask.Flask(__name__, static_url_path='')
+app = flask.Flask(__name__, static_url_path="")
 
 # this route is not needed in production
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
+    """ root of the site """
     return app.send_static_file("html/index.html")
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=False)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=False)
